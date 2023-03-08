59cd42ca25ba990361a9e856671ed63f97e1a127
Anette Johansen <evaajo@stud.ntnu.no>
2023-03-08 13:51:29 +0100

    #2 set up strategy pattern, set up different bullets
    Co-authored-by: Ingebjorg <ingebsto@stud.ntnu.no>

---
+++ core/src/com/ratattack/game/entity/components/AngleBullet.java
@@ -0,0 +1,8 @@
+package com.ratattack.game.entity.components;
+
+public class AngleBullet implements IBulletBehavior {
+    @Override
+    public void shoot() {
+        //Her må logikk for Angle bullet legges til:) endre retning på 2 av 3 kuler:)
+    }
+}
---
+++ core/src/com/ratattack/game/entity/components/TrippelBullet.java
@@ -0,0 +1,8 @@
+package com.ratattack.game.entity.components;
+
+public class TrippelBullet implements IBulletBehavior {
+    @Override
+    public void shoot() {
+        //Her må logikk for trippel bullett legges til. trekke fra 3X health ved hit.
+    }
+}
