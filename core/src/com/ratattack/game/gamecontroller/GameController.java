78688def23b524718ce8cb9cbf15bf0f8ad1e7ec
ingval <ingvild.almasbakk@gmail.com>
2023-03-24 12:59:33 +0100

    #4 vet da faen hva jeg driver med

--- core/src/com/ratattack/game/gamecontroller/GameController.java
+++ core/src/com/ratattack/game/gamecontroller/GameController.java
@@ -2,6 +2,9 @@ package com.ratattack.game.gamecontroller;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameController {
 
     //Model
@@ -9,7 +12,14 @@ public class GameController {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public GameController() {
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
 
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user);
     }
 
     public void update() {
