4f660e9a3b0c5ab7c3a9877b48f17eb5982f2c13
ingval <ingvild.almasbakk@gmail.com>
2023-04-12 10:37:42 +0200

    #19 La til GameSettings for static final variabler

---
+++ core/src/com/ratattack/game/GameSettings.java
@@ -0,0 +1,8 @@
+package com.ratattack.game;
+
+public class GameSettings {
+    public static final long ratSpawnrate = 2000;
+    public static final long grandChildSpawnrate = 2000;
+    public static final int laneNr = 4;
+    public static boolean debug = false; //OBS: ShapeRenderer fører til OutOfMemoryError når programmet har kjørt lenge
+}
