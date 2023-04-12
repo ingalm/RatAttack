91f925d63495e3ef269417e2e5d4b36e3ad5cbdd
Marthth <marthth@stud.ntnu.no>
2023-04-12 16:02:57 +0200

    Highscore list i screen funker og ser bra ut

--- core/src/com/ratattack/game/Highscore.java
+++ core/src/com/ratattack/game/Highscore.java
@@ -15,7 +15,7 @@ public class Highscore {
     public Highscore(FirebaseInterface _FBIC) {
         this._FBIC = _FBIC;
         font = new BitmapFont();
-        font.getData().setScale(2f);
+        font.getData().setScale(6f, 6f);
         fetchHighscores();
     }
 
@@ -31,10 +31,12 @@ public class Highscore {
     }
 
     public void render(SpriteBatch batch) {
-
+        int xPosition = Gdx.graphics.getWidth() / 2 - 400;
+        int yPosition = Gdx.graphics.getHeight() - 150;
+        font.draw(batch, "HIGHSCORE LIST", xPosition, yPosition);
         for (int i = 0; i < getNumberOfElements(highscores)  ; i++) {
-            int xPos = Gdx.graphics.getWidth() / 2 - 30;
-            int yPos = Gdx.graphics.getHeight() - 100 - (i * 50);
+            int xPos = Gdx.graphics.getWidth() / 2 - 350;
+            int yPos = Gdx.graphics.getHeight() - 300 - (i * 100);
             String text = (i + 1) + ". " + highscores.get(i).toString();
             font.draw(batch, text, xPos, yPos);
         }
