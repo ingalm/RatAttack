577d3e2488e1699b4d21616bb39e36c948eebd57
Tuva Djupvik <tuvadjupvik@dhcp-10-220-215-138.wlan.ntnu.no>
2023-04-13 14:50:40 +0200

    Make grandchild move on screen. Rat and grandchild enter outside screen, and is removed outside screen.

--- core/src/com/ratattack/game/model/components/BalanceComponent.java
+++ core/src/com/ratattack/game/model/components/BalanceComponent.java
@@ -5,4 +5,8 @@ import com.badlogic.ashley.core.Component;
 public class BalanceComponent implements Component {
 
     int balance;
+
+    public void setBalance(int x) {
+        this.balance = x;
+    }
 }
--- core/src/com/ratattack/game/model/system/SpawnSystem.java
+++ core/src/com/ratattack/game/model/system/SpawnSystem.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -64,8 +65,8 @@ public class SpawnSystem extends IteratingSystem {
 
         //Position is not randomized yet, should be added
         PositionComponent position = rat.getComponent(PositionComponent.class);
-        position.x = 100;
-        position.y = 1000;
+        position.x = 50;
+        position.y = 1500;
 
         VelocityComponent velocity = rat.getComponent(VelocityComponent.class);
         Random random = new Random();
@@ -77,7 +78,7 @@ public class SpawnSystem extends IteratingSystem {
         velocity.y = -randomNumber;
 
         BoundsComponent bounds = rat.getComponent(RectangleBoundsComponent.class);
-        bounds.setSize(texture.getWidth(), (2*texture.getHeight())/3);
+        bounds.setSize(2*(texture.getWidth()/3), (texture.getHeight()));
         bounds.setCenter(position.x, position.y);
 
         rat.getComponent(HealthComponent.class).setHealth(300);
@@ -87,13 +88,36 @@ public class SpawnSystem extends IteratingSystem {
 
     private void spawnGrandChild() {
         Entity grandChildEntity = new Entity();
-        //grandChildEntity.add(new SpriteComponent());
+        grandChildEntity.add(new SpriteComponent());
         grandChildEntity.add((new VelocityComponent()));
         grandChildEntity.add(new PositionComponent());
         grandChildEntity.add(new HealthComponent());
         grandChildEntity.add(new BalanceComponent());
+        grandChildEntity.add(new RectangleBoundsComponent());
 
         //Add position, velocity, health, balance and sprite
+        Texture texture = new Texture("grandchild.png");
+        grandChildEntity.getComponent(SpriteComponent.class).sprite = new Sprite(texture);
+
+        //Position is not randomized yet, should be added
+        PositionComponent position = grandChildEntity.getComponent(PositionComponent.class);
+        position.x = 400;
+        position.y = 1500;
+
+        VelocityComponent velocity = grandChildEntity.getComponent(VelocityComponent.class);
+        Random random = new Random();
+        int min = 1;
+        int max = 10;
+        int randomNumber = random.nextInt(max - min + 1) + min;
+
+        velocity.x = 0;
+        velocity.y = -randomNumber;
+
+        BoundsComponent bounds = grandChildEntity.getComponent(RectangleBoundsComponent.class);
+        bounds.setSize(2*(texture.getWidth()/3), (texture.getHeight()));
+        bounds.setCenter(position.x, position.y);
+
+        grandChildEntity.getComponent(BalanceComponent.class).setBalance(500);
 
         engine.addEntity(grandChildEntity);
     }
