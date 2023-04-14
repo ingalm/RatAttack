8468665b05bc7b183f04b86ea373e22ec43e256e
ingval <ingvild.almasbakk@gmail.com>
2023-04-14 13:42:50 +0200

    #27 Umm tror jeg har klart det nå, men må sjekke når vi har balanse som endres.

--- core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
+++ core/src/com/ratattack/game/model/shootingStrategy/ShootingStrategy.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public interface ShootingStrategy {
 
     ShootingStrategy[] strategies = {new NormalBulletStrategy(), new BigBulletStrategy(), new FastBulletStrategy(), new FreezeBulletStrategy(), new TripleBulletStrategy()};
+    int[] prices = {100, 200, 300, 400, 500};
 
     void shoot(int x, int y);
 }
